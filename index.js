55b07547d856da5ad8eb90a8688d41c8e7136cc5
bindothstuff <bindothstuff@gmail.com>
2026-04-09 09:52:55 +0500

    made changes in master

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 console.log('hello world')
 console.log('hello world 1')
+
+const userAge = 35
+if (userAge >= 18) {
+    console.log('Вы совершенно летний')
+} else {
+    console.log('Вы не совершенно летний')
+}
