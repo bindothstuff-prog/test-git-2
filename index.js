b44b6d68f99ec7e464183e95716bef10f4a8030e
bindothstuff <bindothstuff@gmail.com>
2026-04-09 09:48:17 +0500

    made changes in feature/main-page

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// master
+
 console.log('hello world')
-console.log('hello world 1')
+
+// develop 
+
+console.log('hello javascript')
+
+// develop -> master
+
+const age = 20 
+if (age > 18) {
+    console.log('Вы совершенно летний')
+} else {
+    console.log('Вы не совершенно летний')
+}
